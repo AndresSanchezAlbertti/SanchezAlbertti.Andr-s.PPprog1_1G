da208c9435f4dbb2978ab3bfdd7c237c8b6a4629
AndresSanchezAlbertti <108681032+AndresSanchezAlbertti@users.noreply.github.com>
2022-10-24 20:04:02 -0300

    Add files via upload
    Profesor no habia guardado el archivo u estaba vacio, este va. Disculpas

--- parcial Practica.c
+++ parcial Practica.c
@@ -10,8 +10,55 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+typedef struct{
+	int id;
+	char nombre[20];
+	char tipo;
+	float efectividad;
+}eVacuna;
+int aplicarDescuento(int precio);
+int reemplazarCaracteres(char array[], char carUno, char carDos);
+int ordenarEstructuraTipoEfectividad(eVacuna x, tam);
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	int precio = 100;
+	char nombre[10] = "Banana";
+	aplicarDescuento(precio);
+	printf("%d", precio);
+	reemplazarCaracteres(nombre, 'a', 'e');
+
 	return EXIT_SUCCESS;
+}
+
+
+int aplicarDescuento(int precio){
+	int precioTotal;
+
+	precioTotal = precio - (precio * 5)/100;
+	return precioTotal;
+}
+
+int reemplazarCaracteres(char cadena[], char carUno, char carDos){
+	int cantidad = 0;
+	if(cadena != NULL ){
+		for(int i= 0; i < sizeof(cadena); i++ ){
+			if(strcmp(cadena[i], carUno)>0){
+				cadena[i] = carDos;
+				cantidad++;
+			}
+		}
+	}
+	return cantidad;
+}
+int ordenarEstructuraTipoEfectividad(eVacuna x,tam){
+	int todoOk = 0;
+
+	for(int i = 0; i<tam; i++){
+		for(int j = i+1; j<tam; j++){
+			if(x[i].tipo > x.tipo[j])||
+					(x[i].tipo == x[j].tipo)&& (x[i].tipo < x[j].tipo){
+
+
+			}
+		}
+	}
 }
